cf6483fcba0dcfc8b8b4e3ecb7ef980f4f09c975
Patrick Badiang <patrickvyn@gmail.com>
2023-09-12 15:48:47 -0400

    Added Venmo

--- src/components/pages/mainPage/gridComponents/BottomFooter.js
+++ src/components/pages/mainPage/gridComponents/BottomFooter.js
@@ -12,7 +12,7 @@ import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import Youtube from '@mui/icons-material/SubscriptionsOutlined';
 import Instagram from '@mui/icons-material/CameraAltOutlined';
-
+import Venmo from '@mui/icons-material/AttachMoney';
 
 
 function BottomFooter() {
@@ -20,6 +20,7 @@ function BottomFooter() {
     const faceBook = 'https://www.facebook.com/fasavirginiatech';
     const youTube = 'https://www.youtube.com/VTFASAHokies';
     const instagram = 'https://www.instagram.com/vt_fasa/';
+    const venmo = 'https://account.venmo.com/u/VTFASA';
 
     return (
         <Grid container xs = {12}>     
@@ -42,6 +43,12 @@ function BottomFooter() {
                     startIcon = {<Youtube/>}>
                         <Typography variant = 'body1' color = "white">
                             Youtube
+                        </Typography>
+                    </Button>
+                    <Button href= {venmo}  target="_blank" rel="noopener" 
+                    startIcon = {<Venmo/>}>
+                        <Typography variant = 'body1' color = "white">
+                            Venmo
                         </Typography>
                     </Button>
                 </ButtonGroup>
